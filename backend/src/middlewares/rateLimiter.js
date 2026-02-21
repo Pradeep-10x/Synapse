2d21548667e0b6fde098fa3f122619ffab381238
Pradeep Singh <pradeep2ndsingh@gmail.com>
2026-02-21 13:50:48 +0530

    Update rate limiter to allow 60 requests per minute

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 15 minutes
-	limit: 5, // 
+	windowMs: 1 * 60 * 1000, // 1 min
+	limit: 60, // 
 	standardHeaders: 'draft-8', 
 	legacyHeaders: false, 
 	ipv6Subnet: 56, 
