7714b3869789e2f8a338cd79c913fd18fb80f930
Pradeep <pradeep2ndsingh@gmail.com>
2026-09-22 16:25:00 +0530

    chore: drop unused import in reel controller

--- backend/src/controllers/reel.controller.js
+++ backend/src/controllers/reel.controller.js
@@ -2,7 +2,6 @@ import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { Follow } from "../models/follow.model.js";
-import {User} from '../models/user.model.js';
 import { Notification } from "../models/notification.model.js";
 import { emitToUser } from '../utils/socketEmitters.js';
 import {uploadonCloudinary} from '../utils/cloudinary.js';
